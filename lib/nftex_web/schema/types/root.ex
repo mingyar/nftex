8c6b28ce8e5f3b4526021df4a50d4d89b34d3453
Mingyar Furtado <mingyar.furtado@gmail.com>
2022-09-15 20:44:15 -0300

    GraphQL log middleware

--- lib/nftex_web/schema/types/root.ex
+++ lib/nftex_web/schema/types/root.ex
@@ -3,6 +3,7 @@ defmodule NftexWeb.Schema.Types.Root do
 
   alias NftexWeb.Schema.Types
   alias NftexWeb.Resolvers.Art, as: ArtResolver
+  alias NftexWeb.Middlewares.Log
 
   import_types Types.Art
   import_types Types.Custom.UUID4
@@ -12,6 +13,7 @@ defmodule NftexWeb.Schema.Types.Root do
     field :art, type: :art do
       arg :id, non_null(:uuid4)
       resolve &ArtResolver.get/2
+      middleware Log
     end
   end
 
@@ -20,6 +22,7 @@ defmodule NftexWeb.Schema.Types.Root do
     field :create_art, type: :art do
       arg :input, non_null(:create_art_input)
       resolve &ArtResolver.create/2
+      middleware Log
     end
   end
 end
